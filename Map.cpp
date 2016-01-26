19e3d8ac85fa0b1f6ace07e8425f38218ab91a22
Jeannie Chiem <gnyorly@gmail.com>
2016-01-26 00:48:56 -0800

    Update Map.cpp
    fixed erase()

--- Map.cpp
+++ Map.cpp
@@ -63,7 +63,7 @@ bool Map::update(const KeyType& key, const ValueType& value)
 	if (contains(key))
 	{
 		Node* p;
-		for (p = head; p != nullptr && p->m_key != key; p = p->m_next);
+		for (p = head; p->m_key != key; p = p->m_next);
 		p->m_value = value;
 		return true;
 	}
@@ -86,29 +86,28 @@ bool Map::erase(const KeyType& key)
 	if (contains(key))
 	{
 		Node* p;
-		for (p = head; p != nullptr && p->m_key != key; p = p->m_next);
+		for (p = head; p->m_key != key; p = p->m_next);
 		if (size() == 1)
 		{
-			delete p;
+			head = nullptr;
+			tail = nullptr;
 		}
-		else if (p = head)
+		else if (p == head)
 		{
 			head = p->m_next;
 			(p->m_next)->m_prev = p->m_prev;
-			delete p;
 		}
-		else if (p = tail)
+		else if (p == tail)
 		{
 			tail = p->m_prev;
 			(p->m_prev)->m_next = p->m_next;
-			delete p;
 		}
 		else
 		{
 			(p->m_prev)->m_next = p->m_next;
 			(p->m_next)->m_prev = p->m_prev;
-			delete p;
 		}
+		delete p;
 		return true;
 	}
 	else
@@ -152,11 +151,19 @@ int main()
 	cool.erase(500);
 	if (cool.contains(79979))
 		cout << "here" << endl;
+	if (!cool.contains(500))
+		cout << "not here" << endl;
 	if (cool.size() == 2)
 		cout << "size 2" << endl;
 	cool.insertOrUpdate(23543, 6999);
-	if (cool.empty())
-		cout << "empty" << endl;
 	if (cool.size() == 2)
 		cout << "size 2" << endl;
+	cool.erase(23543);
+	if (cool.size() == 1)
+		cout << "size 1" << endl;
+	cool.erase(79979);
+	if (cool.size() == 0)
+		cout << "size 0" << endl;
+	if (cool.empty())
+		cout << "empty" << endl;
 }
